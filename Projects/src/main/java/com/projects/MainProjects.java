14a2de3101589eb5cb1c8741a9b19cf720bfa71e
waxif <waxif.1@gmail.com>
2024-10-02 00:21:29 +0400

    Microservices

---
+++ Projects/src/main/java/com/projects/MainProjects.java
@@ -0,0 +1,11 @@
+package com.projects;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class MainProjects {
+    public static void main(String[] args) {
+        System.out.print("Hello im Projects!");
+        SpringApplication.run(MainProjects.class, args);
+    }
+}
